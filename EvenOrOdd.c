698c8c67224d54d072578354a8bd6761be1fd28b
GingaKogen Beer <year0120@gmail.com>
2019-09-03 17:03:16 +0900

    add conditional operator

--- EvenOrOdd.c
+++ EvenOrOdd.c
@@ -1,4 +1,4 @@
-//
+﻿//
 //  偶数か奇数か
 //  以下を参考に作成
 //  https://jp.quora.com/puroguramingu-deha-if-bun-wo-shi-wa-zu-ni-kisuu-ka-guusuu-wo-dou-ya-tte-fun-ka-ri-masu-ka
@@ -8,14 +8,16 @@
 
 int main(void){
 
-    unsigned int in_;
+/*   AND    */
+    int in_;
     char Dec_[2][5] = { "Even", "Odd" };
 
-    printf("input positive integer\n");
+    printf("input integer\n");
     scanf("%d", &in_);
 
     printf("%s\n", Dec_[in_ & 0x01]);
 
+/*   三項演算子    */
+    (in_ & 0x01) > 0 ? printf("Odd\n") : printf("Even\n");
     return 0;
 }
-
