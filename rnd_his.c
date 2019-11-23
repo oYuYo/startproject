02e8ee673b3af605033b96ecbe72541818bcf27e
Gingakogen Beer <year0120@gmail.com>
2019-11-23 23:55:01 +0900

    upload

---
+++ rnd_his.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+#include <time.h>
+#define PI 3.1515926535897993238
+int main(void){
+    int i;
+    int a[10];
+    srand( (unsigned int)time(NULL) );
+    for(i=0; i< 10; i++) a[i] = 0;
+    for(i=0; i<100; i++) a[rand() % 10]++;
+    for(i=0; i< 10; i++) printf("%d : %d\n", i, a[i]);
+
+    for(i=0; i< 10; i++) a[i] = 0;
+    for(i=0; i<100; i++){
+        a[rand() % 10]++;
+    }
+ 
+ 
+
+
+
+    return 0;
+}
