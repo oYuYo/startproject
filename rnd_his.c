460e7ff0b6a8ea3d27cc1859647206880c756d0f
Gingakogen Beer <year0120@gmail.com>
2019-11-24 00:21:22 +0900

    upload

--- rnd_his.c
+++ rnd_his.c
@@ -11,15 +11,5 @@ int main(void){
     for(i=0; i< 10; i++) a[i] = 0;
     for(i=0; i<100; i++) a[rand() % 10]++;
     for(i=0; i< 10; i++) printf("%d : %d\n", i, a[i]);
-
-    for(i=0; i< 10; i++) a[i] = 0;
-    for(i=0; i<100; i++){
-        a[rand() % 10]++;
-    }
- 
- 
-
-
-
     return 0;
 }
