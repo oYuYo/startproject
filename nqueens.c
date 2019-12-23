4ef67451989d2e9b29bfe9f893563ced2bdcdb74
Gingakogen Beer <year0120@gmail.com>
2019-12-23 23:35:44 +0900

    upload

---
+++ nqueens.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 8
+int a[N], b[2*N-1], c[2*N-1], x[N];
+void found(void){
+    int i, j;
+    static int solution = 0;
+    printf("\nresult %d\n", ++solution);
+    for(i=0; i<N; i++){
+        for(j=0; j<N; j++){
+            if(x[i] == j) printf(" Q");
+            else printf(" .");
+        }
+        printf("\n");
+    }
+}
+void try(int i){
+    int j;
+    for(j=0; j<N; j++){
+        if(a[j] && b[i+j] && c[i-j+N-1]){
+            x[i] = j;
+            if(i<N-1){
+                a[j] = b[i+j] = c[i-j+N-1] = 0;
+                try(i+1);
+                a[j] = b[i+j] = c[i-j+N-1] = 1;
+            }else found();
+        }
+    }
+}
+int main(void){
+    int i;
+    for(i=0; i<N; i++) a[i] = 1;
+    for(i=0; i<2*N-1; i++) b[i] = c[i] = 1;
+    try(0);
+    return 0;
+}
