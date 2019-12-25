3958414bc5934ac1de2b99af3ea5ecd8ae7e7a3f
Gingakogen Beer <year0120@gmail.com>
2019-12-25 23:38:13 +0900

    upload

---
+++ shellsort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 10
+void shellsort(int n, int a[]){
+    int h, i, j;
+    int t;
+    h = 13;
+    while(h < n) h = 3 * h + 1;
+    h /= 9;
+    while(h > 0){
+        for(i=0; i<n; i++){
+            t = a[i];
+            for(j=i-h; j>=0 && a[j]>t; j-=h)
+                a[j+h] = a[j];
+            a[j+h] = t;
+        }
+        h /= 3;
+    }
+}
+int main(void){
+    int x[10];
+    srand((unsigned int)time(NULL));
+    for(int i=0; i<N; i++){ x[i]=rand() % 10; printf("%d\t%d\n", i, x[i]); }
+    shellsort(N, x);
+    for(int i=0; i<N; i++) printf("%d\t%d\n", i, x[i]);
+    return 0;
+}
