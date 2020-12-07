c90153d4254c084a54511c92c30db3bc87ea05e1
gingakogenbeer <year0120@gmail.com>
2020-12-07 23:15:22 +0900

    up

--- ruby/m-solutions_c.rb
+++ ruby/m-solutions_c.rb
@@ -1,11 +1,10 @@
 n, k = gets.split.map &:to_i
 a = gets.split.map &:to_i
 s = 0
-for i in 0..k-1
-  s += a[i]
-end
-(k).upto(n-1){ |i|
-  t = (s-a[i-k]) + a[i]
-  puts t>s ? "Yes" : "No"
-  s = t
+(n-k).times{ |i|
+  if a[i] < a[i+k]
+    puts "Yes"
+  else
+    puts "No"
+  end
   }
