1a2f6b778b23ccdda87cc422abb774693ddec8d3
oYuYo <year0120@gmail.com>
2025-05-05 01:22:45 +0900

    up

---
+++ csharp/webapi-sample/WebAPI/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+namespace WebAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        
+        builder.Services.AddDbContextFactory<PubsDbContext>(opt =>
+        {
+            if (builder.Environment.IsDevelopment())
+            {
+                opt = opt.EnableSensitiveDataLogging().EnableDetailedErrors();
+            }
+            opt.UseSqlServer(
+                builder.Configuration.GetConnectionString("PubsDbContext"),
+                providerOptions =>
+                {
+                    providerOptions.EnableRetryOnFailure();
+                });
+        });
+
+        var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(name: MyAllowSpecificOrigins,
+                policy =>
+                {
+                    policy.WithOrigins("https://localhost:7148").AllowAnyHeader().AllowAnyMethod();
+                });
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors(MyAllowSpecificOrigins);
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
